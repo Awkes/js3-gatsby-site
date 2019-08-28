266f3244c941d381543098dd28550c94b44f3003
Andreas Åkerlöf <andreas@akerlof.se>
2019-08-28 10:49:46 +0200

    Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Container from "../components/container";
 // import {Link} from "gatsby";
 import Header from "../components/header";
 import Menu from "../components/menu";
@@ -7,8 +8,10 @@ export default() => (
   <div style={{ color: `purple`}}>
     {/* <Link to="/contact/">Contact</Link> */}
     <Menu activeLink="home" />
-    <Header headerText="Hello Gatsby!" />
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <Container>
+      <Header headerText="Hello Gatsby!" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </Container>
   </div>
 )
